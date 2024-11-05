3d576c1d300316f385288283c92c5de178b80ecf
Murillo Kauê Ribeiro <84983717+kaeuem22@users.noreply.github.com>
2024-11-05 19:43:14 -0300

    Add files via upload

--- runSQL.sql
+++ runSQL.sql
@@ -8,3 +8,20 @@ CREATE TABLE produto (
   Quantidade int NOT NULL,
   PRIMARY KEY (Id)
 );
+
+CREATE TABLE historico (
+  Id int(11) NOT NULL AUTO_INCREMENT,
+  Nome varchar(60) DEFAULT NULL,
+  Preco double NOT NULL,
+  Quantidade int NOT NULL,
+  data_criacao TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (Id)
+);
+
+CREATE TABLE usuario (
+         id INT AUTO_INCREMENT PRIMARY KEY,
+        nome VARCHAR(100) NOT NULL,
+         senha VARCHAR(100) NOT NULL
+     );
+     
+     INSERT INTO usuario (nome, senha) VALUES ('admin', 'mudar123');
